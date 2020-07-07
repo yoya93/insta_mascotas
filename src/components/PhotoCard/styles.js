84c75cabcb7ee73252693dcbbff7dac0e0579b75
jorge <jorgelopezgonzalez474@gmail.com>
2020-07-06 23:48:58 -0400

    add local storage and likes button

--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import { fadeIn } from "../../styles/animation";
 
+export const Article = styled.article`
+  min-height: 200px;
+`;
 export const ImgWrapper = styled.div`
   display: block;
   border-radius: 10px;
