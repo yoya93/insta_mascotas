3980f8129530f38e36e32649f17fafb0a19ff0e0
jorge <jorgelopezgonzalez474@gmail.com>
2020-07-08 12:32:47 -0400

    added get Api photocard and Linear Progress from  material -i

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,12 +1,33 @@
-import React from "react";
-import { photos } from "../../api/db.json";
+import React, { useEffect, useState } from "react";
+
 import { PhotoCard } from "../PhotoCard";
+import { API } from "aws-amplify";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
 export const ListOfPhotoCards = () => {
-  return (
+  const [state, setState] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const getPlans = async () => {
+      try {
+        const photosData = await API.get("amplifyrestapi", "/mascot/photocard");
+        setState(photosData.photocard);
+        setLoading(true);
+      } catch (err) {
+        console.log("error fetching from Lambda API");
+      }
+    };
+
+    console.log(state);
+
+    getPlans();
+  }, []);
+  return !loading ? (
+    <LinearProgress />
+  ) : (
     <div>
       <ul>
-        {photos.map((photo) => (
+        {state.map((photo) => (
           <li key={photo.id}>
             <PhotoCard {...photo} />
           </li>
