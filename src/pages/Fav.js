4a36c08a6a90889bf3f2d205ae179ffcffc941fc
jorge <jorgelopezgonzalez474@gmail.com>
2020-08-10 15:35:38 -0400

    Added pages Fav

--- src/pages/Fav.js
+++ src/pages/Fav.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import { PhotoCard } from "../components/PhotoCard";
+import { API } from "aws-amplify";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
-export const Fav = () => {
-  return (
-    <div>
-      <h1>FAV</h1>
-    </div>
+export const Fav = (props) => {
+  const [state, setState] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const photosData = await API.get("mascots", "/mascots/photocard");
+        setState(photosData.data);
+        setLoading(true);
+      } catch (err) {
+        console.log("error fetching from Lambda API");
+      }
+    };
+
+    getData();
+  }, []);
+
+  return !loading ? (
+    <LinearProgress />
+  ) : (
+    <Fragment>
+      <ul>
+        {state.map(
+          (photo) =>
+            window.localStorage.getItem(`like-${photo.id}`) === "true" && (
+              <li key={photo.id}>
+                <PhotoCard {...photo} />
+              </li>
+            )
+        )}
+      </ul>
+    </Fragment>
   );
 };
